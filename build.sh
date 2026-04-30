6dd172426adee9b777c5ba8ed3b993af4b04b131
Lakshaya7 <lakshaysharma6197@gmail.com>
2026-04-30 19:49:26 +0530

    Fixed 500 error: added DB signals and makemigrations to build

--- build.sh
+++ build.sh
@@ -2,14 +2,11 @@
 # exit on error
 set -o errexit
 
-# Install required packages
 pip install -r requirements.txt
-
-# Bundle static files for the internet
 python manage.py collectstatic --no-input
 
-# Setup the PostgreSQL database automatically!
-python manage.py makemigrations core
-python manage.py migrate
+# THE FIX: Tell Render to dynamically detect new columns
+python manage.py makemigrations
 
+python manage.py migrate
 python manage.py createsuperuser --noinput || true
